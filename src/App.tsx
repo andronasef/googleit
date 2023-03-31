da99ea942f840e85a32650c56f1eec530e0ed89a
Andrew Nasef <androamazing@gmail.com>
2023-03-31 21:22:29 +0200

    Basic Google Search Box UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import IcRoundClose from '~icons/ic/round-close';
+import MaterialSymbolsSearchRounded from '~icons/material-symbols/search-rounded';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [query, setQuery] = useState('');
+
+  const clearQuery = () => setQuery('');
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div className="flex flex-col justify-center items-center min-h-screen gap-5 w-full">
+        <img
+          src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+          alt=""
+          width="200"
+        />
+        <div className="relative w-[90%] md:max-w-[450px] hover:shadow-xl focus-visible:shadow-xl">
+          <MaterialSymbolsSearchRounded className="absolute top-0 bottom-0 m-auto left-4 text-[#9AA0A6]" />
+          <input
+            type="text"
+            className="flex-grow w-full border-[#000*90] border-2 border-solid rounded-full px-10 py-[.45rem] focus-visible:outline-0 overflow-hidden  "
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <button
+            className={
+              (query.length > 0 ? 'visible' : 'opacity-0 invisible') +
+              ' ' +
+              'absolute top-0 bottom-0 m-auto right-5 text-[#9AA0A6]'
+            }
+            onClick={clearQuery}
+          >
+            <IcRoundClose />
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
